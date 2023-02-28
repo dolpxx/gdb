c15556488269e4f8090b575a536c020ceeb5999b
dolpxx <s12102202190@toyo.jp>
2023-03-01 01:30:06 +0900

    changed main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print(42)
+import json
+
+ponya = {"Atomaru": "C", "Kanbara": "AA",
+         "Hamuko": "E", "Konoha": "B", "Mario": "F"}
+
+
+with open(f"data/sample.json", mode="w") as f:
+    json.dump(ponya, f, indent=4)
